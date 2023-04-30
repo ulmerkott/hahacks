e033ba28eb38c363ae33a6c8c80ee48a204e08e6
Eric Svärd <eric.svard@gmail.com>
2023-04-30 08:44:00 +0200

    Refactor a bit and add icon_template -> icon attribute renaming

--- convert_to_new_template_format.py
+++ convert_to_new_template_format.py
@@ -1,18 +1,60 @@
 #!/bin/env python3
 
 from ruamel.yaml import YAML
+from ruamel.yaml.comments import CommentedMap
+
 import sys
 import uuid
 import argparse
+from dataclasses import dataclass
 
 parser = argparse.ArgumentParser(description="""
 Convert old format template sensors to the new template format in configuration.
 Friendly_name will be added as attribute to the new format.
-"value_template" will be converted to "state".
+- "value_template" will be converted to "state".
+- "icon_template" will be converted to "icon".
 Everything else will be moved as is to the new format.
 """)
 parser.add_argument("config_file")
 parser.add_argument("-d", "--dry-run", action="store_true")
+
+@dataclass
+class LegacyTemplateSensorConverter():
+    sensor: CommentedMap
+
+    # Map of attributes of which name has changed in the new format.
+    ATTR_NAME_CONVERSION_TABLE = {
+        "value_template": "state",
+        "icon_template": "icon"
+    }
+
+    def new_sensor(self):
+        new_sensors = []
+        ses = self.sensor.get("sensors")
+        for sensor_name in ses:
+            ses[sensor_name]["unique_id"] = ses[sensor_name].get("unique_id", str(uuid.uuid4()))
+            new_sensor = {
+                "name": sensor_name,
+                "state": ses[sensor_name]['value_template'],
+                "attributes": {
+                    "friendly_name": ses[sensor_name].get("friendly_name", sensor_name)
+                }
+            }
+
+            for old_attr, new_attr in self.ATTR_NAME_CONVERSION_TABLE.items():
+                if attr := ses[sensor_name].get(old_attr):
+                    new_sensor[new_attr] = attr
+                    del ses[sensor_name][old_attr]
+
+            new_sensor.update(ses[sensor_name])
+            new_sensors.append({
+                "sensor": [
+                    new_sensor
+                    ]
+            })
+
+        return new_sensors
+
 
 def main():
     args = parser.parse_args()
@@ -26,28 +68,10 @@ def main():
         ha_config = yaml.load(fd)
     old_sensors = ha_config.get("sensor")
     new_sensors = ha_config.get("template", [])
-
     for sensor in old_sensors[:]:
         if sensor.get("platform") != "template":
             continue
-        ses = sensor.get("sensors")
-        for se in ses:
-            ses[se]["unique_id"] = ses[se].get("unique_id", str(uuid.uuid4()))
-            new_sensor = {
-                "name": se,
-                "state": ses[se]['value_template'],
-                "attributes": {
-                    "friendly_name": ses[se].get("friendly_name", se)
-                }
-            }
-            del ses[se]["friendly_name"]
-            del ses[se]["value_template"]
-            new_sensor.update(ses[se])
-            new_sensors.append({
-                "sensor": [
-                    new_sensor
-                    ]
-            })
+        new_sensors += LegacyTemplateSensorConverter(sensor).new_sensor()
         old_sensors.remove(sensor)
 
     ha_config["template"] = new_sensors
